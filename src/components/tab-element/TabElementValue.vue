d5b6c5598a3fe809acc8cde64892f03daf28f796
Shamsingh Hajeri <105161301+ShamHajeri@users.noreply.github.com>
2024-02-19 15:16:49 +0530

    Update Eye btn

---
+++ src/components/tab-element/TabElementValue.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="Tab-ElementValue">
+        <span> {{ data.slice(0,25) }}{{ data.length>25?'...':'' }} </span>
+    </div>
+</template>
+<script>
+
+export default {
+    name:'TabElementValue',
+    data(){
+        return{
+            data:"bjcbaksjbjbsajkbjkasbkjcbsjbjsabkcbsabckjsabsa"
+        }
+    }
+    
+}
+</script>
+<style scoped>
+.Tab-ElementValue{
+    width: max-content;
+    display: flex;
+    border-radius: 4px;
+    border: 2px gray solid;
+    cursor: pointer;
+}
+span{
+    padding: 10px 15px 10px 15px;
+    color: #07701c;
+
+}
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <TanElementKey></TanElementKey>
+    <TabElementKeyValue></TabElementKeyValue>
+    <TabElementValue></TabElementValue>
+</template>
+
+<script>
+import TanElementKey from '../components/tab-element/TabElementKey.vue'
+import TabElementKeyValue from '../components/tab-element/TabElementKeyValue.vue';
+import  TabElementValue from '../components/tab-element/TabElementValue.vue'
+export default{
+
+    name:'MainPage',
+    components:{
+        TanElementKey,
+        TabElementKeyValue,
+        TabElementValue
+    }
+}
+</script>
