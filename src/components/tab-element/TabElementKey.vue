505b3ddd5bb69a9081834d997481b0f66364869a
Shamsingh Hajeri <105161301+ShamHajeri@users.noreply.github.com>
2024-02-19 15:20:57 +0530

    Revert "Revert "Update Eye btn""
    This reverts commit 636947f720954068d416e744713cd173a4a01c69.

---
+++ src/components/tab-element/TabElementKey.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="Tab-ElementKey">
+        <div class="Tab-Content">
+            <span>
+                member
+            </span>
+            <span>
+                (2)
+            </span>
+        </div>
+        <button @click='toggleBtn' v-if=true>
+           {{ eyeHide?`&#x26D2;`:` &#128065;`  }}
+        </button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TanElementKey',
+    data() {
+        return {
+            eyeHide: false
+        }
+
+    },
+    methods: {
+        toggleBtn() {
+            this.eyeHide = !this.eyeHide
+        }
+    }
+}
+
+</script>
+<style scoped>
+.Tab-ElementKey {
+    width: max-content;
+    display: flex;
+    border-radius: 4px;
+    border: 2px gray solid;
+    cursor: pointer;
+}
+
+.Tab-Content {
+    display: inline;
+    padding: 8px;
+
+}
+
+span {
+    padding: 2px 6px 2px 6px;
+    color: #ff6b00;
+}
+
+button {
+    width: 28px;
+    height: auto;
+    border: none;
+    background-color: #dddfe1;
+    ;
+}
+</style>
